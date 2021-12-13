a7eb05e5163bce516fd4bdc3a2d4891f669605eb
Jono <developer@jono.fyi>
2021-12-13 14:32:32 -0800

    Ready for the Visualization

--- src/app.js
+++ src/app.js
@@ -4,28 +4,28 @@ import "./main.css";
 
 export default function App(props) {
 
+  var [textIsVisible, setTextIsVisible] = useState(true);
+  var [vizIsVisible, setVizIsVisible] = useState(true);
   var [texts, setTexts] = useState([{}]);
 
-  useEffect(setup, []);
-
-  function setup() {
-
-  }
-
   function increase() {
-    setTexts(function(text) {
-      var result = text.slice(0);
-      result.push({});
-      return result;
-    });
+    var result = texts.slice(0);
+    result.push({});
+    setTexts(result);
   }
 
   function reduce() {
-    setTexts(function(text) {
-      var result = text.slice(0);
-      result.pop();
-      return result;
-    });
+    var result = texts.slice(0);
+    result.pop();
+    setTexts(result);
+  }
+
+  function toggleText() {
+    setTextIsVisible(!textIsVisible);
+  }
+
+  function toggleVisuals() {
+    setVizIsVisible(!vizIsVisible);
   }
 
   //
@@ -44,17 +44,29 @@ export default function App(props) {
   return (
     <div className="app">
 
-      <div className="content">
+      <div className={ ['view', 'visualization', vizIsVisible ? 'enabled' : ''].join(' ') }>
+
+      </div>
+
+      <div className={ ['view', 'text', textIsVisible ? 'enabled' : ''].join(' ') }>
         { texts.map(render) }
       </div>
 
       <div className="menu">
-        <button onClick={ reduce }>
-          Remove Text Field
-        </button>
         <button onClick={ increase }>
           Add Text Field
         </button>
+        <button onClick={ reduce }>
+          Remove Text Field
+        </button>
+        <span>
+          <input id="text-visibility" type="checkbox" defaultChecked={ textIsVisible } onChange={ toggleText } />
+          <label htmlFor="text-visibility">Text Visible</label>
+        </span>
+        <span>
+          <input id="viz-visibility" type="checkbox" defaultChecked={ vizIsVisible } onChange={ toggleVisuals } />
+          <label htmlFor="viz-visibility">Visuals Visible</label>
+        </span>
       </div>
 
     </div>
